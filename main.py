654fe191eb0db7b88e9d018ddbbb9d0c1e50ee59
Bartanakin <bartanakin@interia.pl>
2022-06-11 20:18:24 +0200

    wewnetrzne problemy algorytmow

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from mimetypes import init
 from scipy.integrate import odeint
+from scipy.integrate import quad
+from scipy.optimize import minimize
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Module main
 
-n = 10
+n = 1
 init_a = np.array([ 1 for i in range(n) ])
 S = 1.2
+P_t = 100
+P_f = 100
+F_C = 100
+x_c = 40
+T_max = 100
+
 
 def T(x: float, a: np.array):
     result = 0
@@ -23,16 +32,27 @@ def diff_count(y,x,a):
     dtdx =  np.sqrt(S)/np.sqrt( T(x,a) - Do(x) )
     return dtdx
 
-def diff_test(y,x):
-    dtdx = x**2
-    return dtdx
+def F(x: float, a: np.array):
+    F_C*T(x,a)
 
-y0 = 0
-x = np.linspace(-40,40,1000)
-y = odeint(diff_count, y0, x,args = (init_a,))
+def objective_function(a: np.array):
+    return P_t*odeint(diff_count,0,[0,x_c],args = (a,))[1][0] + P_f*F_C*quad(T,0,x_c,args=(a,))[0]
 
-plt.plot(x,y,'r-',label='Output (y(t))')
-plt.ylabel('time')
+contraints = []
+N = 100
+delta = x_c/N
+for i in range(N + 1):
+    contraints.append( {'type': 'ineq', 'fun': lambda a: T(delta*i,a) - Do(delta*i)} )
+    # contraints.append( {'type': 'ineq', 'fun': lambda a: T_max - T(delta*i,a)} )
+result = minimize(objective_function,init_a,method='BFGS',constraints=contraints)
+print(result)
+x = np.linspace(0,x_c,1000)
+y = []
+for i in range(1000):
+    y.append(T(x[i],result.x))
+
+print(objective_function(np.array([0.001])))
+plt.plot(x,y,'r-')
+plt.ylabel('engine power')
 plt.xlabel('distance')
-plt.legend(loc='best')
 plt.show()
